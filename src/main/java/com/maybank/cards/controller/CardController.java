bf1539328364bd27f59e43999cf560731be5d945
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 00:05:16 +0530

    Create CardController.java

---
+++ src/main/java/com/maybank/cards/controller/CardController.java
@@ -0,0 +1,10 @@
+package com.maybank.cards.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/card")
+public class CardController {
+
+}
