293cd72c7da548a016f83753db9640cb38d6ee0c
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 00:05:13 +0530

    Create CardsManagementServiceApplication.java

---
+++ src/main/java/com/maybank/cards/CardsManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maybank.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsManagementServiceApplication.class, args);
+	}
+
+}
