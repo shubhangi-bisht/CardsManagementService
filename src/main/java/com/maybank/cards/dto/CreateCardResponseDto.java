b4b6acf256922e76d7993787b5ce3fdb0977a3dc
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 11:23:46 +0530

    Update CreateCardResponseDto.java
    create card response dto

--- src/main/java/com/maybank/cards/dto/CreateCardResponseDto.java
+++ src/main/java/com/maybank/cards/dto/CreateCardResponseDto.java
@@ -1,4 +1,43 @@
 package com.maybank.cards.dto;
 
 public class CreateCardResponseDto {
+    private String name;
+
+    private String accountNumber;
+
+    private LocalDate expiryDate;
+
+    private String status;
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public LocalDate getExpiryDate(){
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate){
+        this.expiryDate = expiryDate;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
 }
