6aba2b8bd37a7a84974b3c69c4b8fc08b3de468d
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 13:36:42 +0530

    Update CardController.java

--- src/main/java/com/maybank/cards/controller/CardController.java
+++ src/main/java/com/maybank/cards/controller/CardController.java
@@ -1,12 +1,15 @@
 package com.maybank.cards.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maybank.cards.dto.CreateCardRequestDto;
 import com.maybank.cards.dto.CreateCardResponseDto;
+import com.maybank.cards.dto.FetchCardDetailsRequestDto;
+import com.maybank.cards.dto.FetchCardDetailsResponseDto;
 import com.maybank.cards.service.CardService;
 
 @RestController
@@ -21,12 +24,29 @@ public class CardController {
     @Autowired
     CardService cardService;
 
-    @PostMapping("/createCard")
+    @PostMapping(value = "/create-card")
     public CreateCardResponseDto createCard(CreateCardRequestDto createCardRequest){
+    	CreateCardResponseDto createCardResponseDto = new CreateCardResponseDto();
         try{
+        	createCardResponseDto = cardService.createCard(createCardRequest);
 
         } catch (java.lang.Exception e) {
             throw new RuntimeException(e);
         }
+        return createCardResponseDto;
     }
-}
+    
+  //Fetch Card Details for a customer
+    @GetMapping(value = "/fetch-card-details")
+    public FetchCardDetailsResponseDto fetchCardDetails(FetchCardDetailsRequestDto fetchCardDetailsRequestDto) {
+    	FetchCardDetailsResponseDto fetchCardDetailsResponseDto = new FetchCardDetailsResponseDto();
+        try{
+        	fetchCardDetailsResponseDto = cardService.fetchCardDetails(fetchCardDetailsRequestDto);
+
+        } catch (java.lang.Exception e) {
+            throw new RuntimeException(e);
+        }
+        return fetchCardDetailsResponseDto;
+    }
+    }
+
