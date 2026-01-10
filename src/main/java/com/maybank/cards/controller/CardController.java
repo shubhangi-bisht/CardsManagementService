144dfcce53425088dfd41dea7fb029016d7ed9f7
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 11:47:09 +0530

    Update CardController.java

--- src/main/java/com/maybank/cards/controller/CardController.java
+++ src/main/java/com/maybank/cards/controller/CardController.java
@@ -1,10 +1,32 @@
 package com.maybank.cards.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.maybank.cards.dto.CreateCardRequestDto;
+import com.maybank.cards.dto.CreateCardResponseDto;
+import com.maybank.cards.service.CardService;
 
 @RestController
 @RequestMapping(value = "/card")
 public class CardController {
 
+    //1. Create card
+    //2. Fetch Card details
+    //3. Update Status of card
+    //4. Fetch all card detail of a customer linked to a particular account number - Pagination
+
+    @Autowired
+    CardService cardService;
+
+    @PostMapping("/createCard")
+    public CreateCardResponseDto createCard(CreateCardRequestDto createCardRequest){
+        try{
+
+        } catch (java.lang.Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
