4e35d6948426fcab03d71c292fbafce12c35d1e0
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 13:36:47 +0530

    Update CardService.java

--- src/main/java/com/maybank/cards/service/CardService.java
+++ src/main/java/com/maybank/cards/service/CardService.java
@@ -1,10 +1,17 @@
 package com.maybank.cards.service;
 
+import org.springframework.stereotype.Service;
+
 import com.maybank.cards.dto.CreateCardRequestDto;
 import com.maybank.cards.dto.CreateCardResponseDto;
+import com.maybank.cards.dto.FetchCardDetailsRequestDto;
+import com.maybank.cards.dto.FetchCardDetailsResponseDto;
 
+@Service
 public interface CardService {
 
     public CreateCardResponseDto createCard(CreateCardRequestDto requestDto);
+    
+    public FetchCardDetailsResponseDto fetchCardDetails(FetchCardDetailsRequestDto requestDto);
 
 }
