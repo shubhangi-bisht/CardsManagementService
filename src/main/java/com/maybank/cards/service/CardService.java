d3501503a9e0780ca19421a9857146fb14ea1e00
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 11:47:25 +0530

    Update CardService.java

--- src/main/java/com/maybank/cards/service/CardService.java
+++ src/main/java/com/maybank/cards/service/CardService.java
@@ -1,4 +1,10 @@
 package com.maybank.cards.service;
 
+import com.maybank.cards.dto.CreateCardRequestDto;
+import com.maybank.cards.dto.CreateCardResponseDto;
+
 public interface CardService {
+
+    public CreateCardResponseDto createCard(CreateCardRequestDto requestDto);
+
 }
