ad746316303b677f72790136ff454633e08648d2
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 16:57:03 +0530

    Update UpdateCardStatusResponseDto.java
    Update card status response DTO modified as per identified requirements

--- src/main/java/com/maybank/cards/dto/UpdateCardStatusResponseDto.java
+++ src/main/java/com/maybank/cards/dto/UpdateCardStatusResponseDto.java
@@ -1,20 +1,15 @@
 package com.maybank.cards.dto;
 
 public class UpdateCardStatusResponseDto {
-    private String status;
 
     private String message;
 
-    public String getStatus(){
-        return status;
-    }
-
-    public void setStatus(String status){
-        this.status = status;
-    }
-
     public void setMessage(String message){
         this.message = message;
+    }
+    
+    public String getMessage() {
+    	return message;
     }
 
 }
