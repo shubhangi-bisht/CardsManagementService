052ffb9c9e8eca73142da21c5fcf8e09c508842d
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 16:56:40 +0530

    Update UpdateCardStatusRequestDto.java
    Update Card Status Request dto modified as per identified requirement

--- src/main/java/com/maybank/cards/dto/UpdateCardStatusRequestDto.java
+++ src/main/java/com/maybank/cards/dto/UpdateCardStatusRequestDto.java
@@ -3,6 +3,8 @@ package com.maybank.cards.dto;
 public class UpdateCardStatusRequestDto
 {
     private String status;
+    
+    private String accountNumber;
 
     public String getStatus(){
         return status;
@@ -11,4 +13,12 @@ public class UpdateCardStatusRequestDto
     public void setStatus(String status){
         this.status = status;
     }
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
 }
