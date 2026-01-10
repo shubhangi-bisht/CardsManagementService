68174df597ff52987783d7bf511a38cc29ad3b43
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 16:57:31 +0530

    Update CardRepository.java
    Native query using JPA added to update card status

--- src/main/java/com/maybank/cards/repository/CardRepository.java
+++ src/main/java/com/maybank/cards/repository/CardRepository.java
@@ -1,13 +1,19 @@
 package com.maybank.cards.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.maybank.cards.entity.CardHolder;
 
 @Repository
 public interface CardRepository extends JpaRepository<CardHolder, String>{
-	
-	
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE CardHolder c SET c.status = :status WHERE c.accountNumber = :accountNumber")
+	int updateStatusByAccountNumber(String accountNumber, String status);
 
 }
