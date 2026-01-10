cecbb566170c18a37e01be6ea1fa1e1ee7d181ce
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 13:36:49 +0530

    Update CardServiceImpl.java

--- src/main/java/com/maybank/cards/serviceImpl/CardServiceImpl.java
+++ src/main/java/com/maybank/cards/serviceImpl/CardServiceImpl.java
@@ -1,16 +1,40 @@
 package com.maybank.cards.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.maybank.cards.dto.CreateCardRequestDto;
 import com.maybank.cards.dto.CreateCardResponseDto;
+import com.maybank.cards.dto.FetchCardDetailsRequestDto;
+import com.maybank.cards.dto.FetchCardDetailsResponseDto;
+import com.maybank.cards.repository.CardRepository;
 import com.maybank.cards.service.CardService;
+import com.maybank.cards.util.Mapper;
 
+@Service
 public class CardServiceImpl implements CardService{
 
+	@Autowired
+	CardRepository cardRepository;
+	
+	@Autowired
+	Mapper mapper;
+	
+	//Create Card for a customer
 	@Override
 	public CreateCardResponseDto createCard(CreateCardRequestDto requestDto) {
+		CreateCardResponseDto createCardResponseDto = new CreateCardResponseDto();
+		cardRepository.save(mapper.toMap(requestDto));
+		return createCardResponseDto;
+	}
+
+	//Fetch Card Details for a customer
+	@Override
+	public FetchCardDetailsResponseDto fetchCardDetails(FetchCardDetailsRequestDto requestDto) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 
 
 }
