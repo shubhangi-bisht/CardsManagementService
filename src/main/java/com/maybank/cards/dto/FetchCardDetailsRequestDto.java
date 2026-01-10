9503ab8e829fe603a19726d7561f9f27ea3ce2d7
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-10 11:24:04 +0530

    Update FetchCardDetailsRequestDto.java
    fetch card details request dto

--- src/main/java/com/maybank/cards/dto/FetchCardDetailsRequestDto.java
+++ src/main/java/com/maybank/cards/dto/FetchCardDetailsRequestDto.java
@@ -4,13 +4,13 @@ import java.time.LocalDate;
 
 public class FetchCardDetailsRequestDto {
 
-    private String name;
+    private String accountNumber;
 
-    public String getName(){
-        return name;
+    public String getAccountNumber(){
+        return accountNumber;
     }
 
-    public void setName(String name){
-        this.name = name;
+    public void setAccountNumber(String accountNumber){
+        this.accountNumber = accountNumber;
     }
 }
