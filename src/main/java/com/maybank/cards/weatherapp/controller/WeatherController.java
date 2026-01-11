50357ee5e0632c1345d857a7d2e0e20aad6ad668
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-11 23:22:16 +0530

    Create WeatherController.java
    Weather API controller added

---
+++ src/main/java/com/maybank/cards/weatherapp/controller/WeatherController.java
@@ -0,0 +1,34 @@
+package com.maybank.cards.weatherapp.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maybank.cards.weather.app.service.WeatherService;
+
+
+
+@RestController
+public class WeatherController {
+
+	private final WeatherService weatherService;
+
+	public WeatherController(WeatherService weatherService) {
+		this.weatherService = weatherService;
+	}
+
+	@GetMapping(value = "/weather", produces = MediaType.APPLICATION_JSON_VALUE)
+
+	public ResponseEntity<String> getWeather(@RequestParam double lat,
+			@RequestParam double lon) {
+
+		String body = weatherService.fetchWeather(lat, lon)
+				.block();
+
+		return ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(body);
+	}
+}
