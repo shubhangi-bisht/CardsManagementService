40f674dbdca407d963eefecf844f8e5607c8160e
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-11 13:58:25 +0530

    Update CardController.java
    try catch removed and Customer exception handling done at controller layer

--- src/main/java/com/maybank/cards/controller/CardController.java
+++ src/main/java/com/maybank/cards/controller/CardController.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,71 +26,60 @@ import com.maybank.cards.dto.UpdateCardStatusResponseDto;
 import com.maybank.cards.service.CardService;
 
 @RestController
-@RequestMapping(value = "/card")
+@RequestMapping("/card")
 public class CardController {
 
-	//1. Create card
-	//2. Fetch Card details
-	//3. Update Status of card
-	//4. Fetch all card detail of a customer linked to a particular account number - Pagination
+    @Autowired
+    private CardService cardService;
 
-	@Autowired
-	CardService cardService;
+    private static final Logger logger = LogManager.getLogger(CardController.class);
 
-	private static final Logger logger =
-	        LogManager.getLogger(CardController.class);
-	
-	//Create Card for a new user
-	@PostMapping(value = "/create-card")
-	public CreateCardResponseDto createCard(@RequestBody CreateCardRequestDto createCardRequest){
-		CreateCardResponseDto createCardResponseDto = new CreateCardResponseDto();
-		try{
-			createCardResponseDto = cardService.createCard(createCardRequest);
+    //****************** CREATE CARD ********************//
+    @PostMapping("/create-card")
+    public ResponseEntity<CreateCardResponseDto> createCard(
+            @RequestBody CreateCardRequestDto request) {
 
-		} catch (java.lang.Exception e) {
-			createCardResponseDto.setMessage("Some exception occured while creating account. "+e);
-		}
-		return createCardResponseDto;
-	}
+        logger.info("Create card request received for account: {}", request.getAccountNumber());
 
-	//Fetch Card Details for a customer
-	@GetMapping(value = "/fetch-card-details")
-	public FetchCardDetailsResponseDto fetchCardDetails(@RequestBody FetchCardDetailsRequestDto fetchCardDetailsRequestDto) {
-		FetchCardDetailsResponseDto fetchCardDetailsResponseDto = new FetchCardDetailsResponseDto();
-		try{
-			logger.info("Account number: "+fetchCardDetailsRequestDto.getAccountNumber());
-			fetchCardDetailsResponseDto = cardService.fetchCardDetails(fetchCardDetailsRequestDto);
+        // Service layer handles exceptions
+        CreateCardResponseDto response = cardService.createCard(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
-		} catch (java.lang.Exception e) {
-			fetchCardDetailsResponseDto.setMessage("Some exception occured while fetch data. "+e);
-		}
-		return fetchCardDetailsResponseDto;
-	}
-	
-	//Method to fetch 10 records at a time
-		@GetMapping(value = "/fetch-cardholder-details")
-		public Page<CardHolderResponseDto> fetchCardHolderDetails(@RequestParam(defaultValue = "0") int page ) {
-			return cardService.fetchCardHolderDetails(page);
+    //****************** FETCH CARD DETAILS ********************//
+    @GetMapping("/fetch-card-details")
+    public ResponseEntity<FetchCardDetailsResponseDto> fetchCardDetails(
+            @RequestBody FetchCardDetailsRequestDto requestDto) {
 
-		}
-	
-	//Update status of the card
-	@PutMapping(value = "/update-card-status")
-	public UpdateCardStatusResponseDto updateCardStatus(@RequestBody UpdateCardStatusRequestDto requestDto) {
-		UpdateCardStatusResponseDto updateCardStatusResponseDto = new UpdateCardStatusResponseDto();
-		try {
-			if(Objects.nonNull(requestDto.getAccountNumber()) 
-					&& Objects.nonNull(requestDto.getStatus())){
-				updateCardStatusResponseDto = cardService.updateCardStatus(requestDto);
-			}
-			else {
-				updateCardStatusResponseDto.setMessage("No Account Number found in request");
-			}
-		}
-		catch(Exception e) {
-			updateCardStatusResponseDto.setMessage("Some exception occured while fetch data. "+e);
-		}
-		return updateCardStatusResponseDto;
-	}
+        logger.info("Fetch card details request received for account: {}", requestDto.getAccountNumber());
+
+
+        // Service layer throws NoDataFoundException if not found
+        FetchCardDetailsResponseDto response = cardService.fetchCardDetails(requestDto);
+        return ResponseEntity.ok(response);
+    }
+
+    //****************** FETCH CARD HOLDERS WITH PAGINATION ********************//
+    @GetMapping("/fetch-cardholder-details")
+    public ResponseEntity<Page<CardHolderResponseDto>> fetchCardHolderDetails(
+            @RequestParam(defaultValue = "0") int page) {
+
+        logger.info("Fetch card holders request received, page={}", page);
+
+        Page<CardHolderResponseDto> response = cardService.fetchCardHolderDetails(page);
+        return ResponseEntity.ok(response);
+    }
+
+    //****************** UPDATE CARD STATUS ********************//
+    @PutMapping("/update-card-status")
+    public ResponseEntity<UpdateCardStatusResponseDto> updateCardStatus(
+            @RequestBody UpdateCardStatusRequestDto request) {
+
+        logger.info("Update card status request received for account: {}", request.getAccountNumber());
+
+        UpdateCardStatusResponseDto response = cardService.updateCardStatus(request);
+        return ResponseEntity.ok(response);
+    }
 }
+
 
