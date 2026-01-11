a07d97628182a6dfe341185e6cc65ea9ee497c4a
shubhangi-bisht <shubhisht1998@gmail.com>
2026-01-12 00:20:50 +0530

    Create Resilience4jConfig.java
    Resilience4j config file added for fall back mechanism

---
+++ src/main/java/com/maybank/cards/util/Resilience4jConfig.java
@@ -0,0 +1,10 @@
+package com.maybank.cards.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class Resilience4jConfig {
+    // Enables @Retry and @CircuitBreaker
+}
